86221a967e401ae552a9e6c0da20fc31f738e862
bbdoc <bbdoc.mail@gmail.com>
2020-12-14 17:34:19 +0100

    Use custom Title in Page Title

--- header.php
+++ header.php
@@ -1,9 +1,20 @@
+<?php
+
+include "./config.php";
+include "./db_connect.php";
+include "./functions.php";
+include "./tooltips.php";
+
+if(isset($custom_title)) { $title = $custom_title; } else { $title = "PoracleWeb"; }
+
+?>
+
 <html translate="no">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PoracleWeb</title>
+  <title><?php echo $title; ?></title>
   <link rel="icon" type="image/x-icon" href="favicon.png"/>
   <link rel="stylesheet" type="text/css" href="css/style.css?v=<?=time();?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -45,13 +56,6 @@
 </head>
 
 <?php
-
-include "./config.php";
-include "./db_connect.php";
-include "./functions.php";
-include "./tooltips.php";
-
-if(isset($custom_title)) { $title = $custom_title; } else { $title = "PoracleWeb"; }
 
 if(isset($_SESSION['username'])) {
 
