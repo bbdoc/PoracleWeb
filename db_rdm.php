3179899dfc5e2f21017358c8c850ccbe779a00e7
bbdoc <bbdoc.mail@gmail.com>
2021-01-02 00:08:41 +0100

    Pokemon ID is 0 in RDM

--- db_rdm.php
+++ db_rdm.php
@@ -67,7 +67,7 @@ function get_raid_bosses() {
 
    $conn = new mysqli($scan_dbhost.":".$scan_dbport, $scan_dbuser, $scan_dbpass, $scan_dbname);
    $sql = "SELECT raid_pokemon_id, raid_pokemon_form, raid_pokemon_evolution, raid_pokemon_costume FROM gym
-           WHERE raid_pokemon_id is not null AND raid_end_timestamp > UNIX_TIMESTAMP(DATE_SUB(now(), INTERVAL 1 DAY))
+           WHERE raid_pokemon_id <> 0 AND raid_end_timestamp > UNIX_TIMESTAMP(DATE_SUB(now(), INTERVAL 1 DAY))
            GROUP BY raid_level, raid_pokemon_id, raid_pokemon_form, raid_pokemon_costume ORDER BY raid_level, raid_pokemon_id;";
    $result = $conn->query($sql);
 
