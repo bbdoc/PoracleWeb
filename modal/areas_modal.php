d6d5948a597f579fbaf55aef0fd2396fc855ba65
George Herbert <georgeherby@hotmail.com>
2021-01-26 20:26:33 +0000

    Add in old way when there is only one group this covers someone who sets up with just one group or no groups at all.

--- modal/areas_modal.php
+++ modal/areas_modal.php
@@ -17,18 +17,34 @@ echo "
 
 $areas = get_areas();
 
-echo "<div class='accordion' id='accordion'>";
 $collapsedState = 'collapsed';
 
-// Do not collapse the only section, if it has no label as this is for user with no group set in their geofences
-if(count(array_keys($areas)) === 1 && array_key_exists("", $areas)){
+// Show no accordions when there is only one list of areas (likely a user with no grouping)
+if(count(array_keys($areas)) === 1){
     $collapsedState = '';
-}
+    $areaList = $areas[0];
+    echo "<ul>\n";
+        sort($areaList);
+        foreach ($areaList as $i => $area) {
+            $area = str_replace(' ', '_', $area);
 
-foreach ($areas as $group => $areaList) {
-    $updatedGroup = str_replace(' ', '_', $group);
+            if (in_array(strtolower($area), $existing_area)) {
+                $checked = 'checked';
+            } else {
+                $checked = '';
+            };
+            echo "<li><input type='checkbox' name='area_$area' id='area_$area' $checked/>\n";
+            echo "<label for='area_$area' style='width:160px;'><font style='font-size:12px;'>$area</font></label>\n";
+            echo "</li>\n";
+        }
+echo "</ul>\n";
+} else {
+    echo "<div class='accordion' id='accordion'>";
 
-    echo "<div class='card'>
+    foreach ($areas as $group => $areaList) {
+        $updatedGroup = str_replace(' ', '_', $group);
+
+        echo "<div class='card'>
              <div class='card-header' id='heading$updatedGroup'>
                   <h5 class='mb-0'>
                 <button class='btn btn-link $collapsedState' type='button' data-toggle='collapse' data-target='#collapse$updatedGroup' aria-expanded='false' aria-controls='collapse$updatedGroup'>
@@ -39,26 +55,26 @@ foreach ($areas as $group => $areaList) {
     
             <div id='collapse$updatedGroup' class='collapse' aria-labelledby='heading$updatedGroup' data-parent='#accordion'>
                 <div class='card-body'>";
-    sort($areaList);
-    echo "<ul>\n";
-    foreach ($areaList as $i => $area) {
-        $area = str_replace(' ', '_', $area);
+        sort($areaList);
+        echo "<ul>\n";
+        foreach ($areaList as $i => $area) {
+            $area = str_replace(' ', '_', $area);
 
-        if (in_array(strtolower($area), $existing_area)) {
-            $checked = 'checked';
-        } else {
-            $checked = '';
+            if (in_array(strtolower($area), $existing_area)) {
+                $checked = 'checked';
+            } else {
+                $checked = '';
+            }
+            echo "<li><input type='checkbox' name='area_$area' id='area_$area' $checked/>\n";
+            echo "<label for='area_$area' style='width:160px;'><font style='font-size:12px;'>$area</font></label>\n";
+            echo "</li>\n";
         }
-        echo "<li><input type='checkbox' name='area_$area' id='area_$area' $checked/>\n";
-        echo "<label for='area_$area' style='width:160px;'><font style='font-size:12px;'>$area</font></label>\n";
-        echo "</li>\n";
+        echo "</ul>\n</div>";
+        echo "</div>";
+        echo "</div>";
     }
-    echo "</ul>\n</div>";
-    echo "</div>";
     echo "</div>";
 }
-echo "</div>";
-
 echo "
         <hr>
         <div class='float-right'>            
