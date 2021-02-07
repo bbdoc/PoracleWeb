002ecc13bb90fc31bf2e25c6f13854d3290513a2
bbdoc <bbdoc.mail@gmail.com>
2021-02-07 23:27:06 +0100

    Send error html at end of action handling

--- actions/invasions.php
+++ actions/invasions.php
@@ -1,56 +1,7 @@
-<html>
+<?php
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PoracleWeb</title>
-  <link rel="icon" type="image/x-icon" href="favicon.png" />
-  <link rel="stylesheet" type="text/css" href="../css/style.css?v=<?= time(); ?>">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
-
-</head>
-
-<body style="background-color:#FFFFFF; color: grey;">
-  <br>
-
-  <?php
-
-#  echo "<table>";
-#      foreach ($_POST as $key => $value) {
-#          echo "<tr>";
-#          echo "<td>";
-#          echo $key;
-#          echo "</td>";
-#          echo "<td>";
-#          echo $value;
-#          echo "</td>";
-#          echo "</tr>";
-#      }
-#  echo "</table>";
-
-  include "../config.php";
-  include "../db_connect.php";
-
-  ?>
-
-  <center>
-    <div style="max-width:90%">
-      <font size=6 color="darkred">Oops...</font><br><br>
-      <img src='../img/homer.png' width=250><br><br>
-      Looks like Something went wrong, you shouldn't be here.<br><br>
-      Please get back to homepage:<br><br>
-      <a href="<?php echo $redirect_url; ?>">
-        <font size=5>PoracleWeb</font>
-      </a>
-    </div>
-  </center>
-
-  <?php
+   include "../config.php";
+   include "../db_connect.php";
 
   // UPDATE INVASIONS
 
@@ -236,4 +187,6 @@
     exit();
 
   }
+
+  include "./action_error.php";
 
