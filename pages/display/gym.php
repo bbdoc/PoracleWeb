04e1658963258ec5222e25463a7c1ff3a3a2c64f
bbdoc <bbdoc.mail@gmail.com>
2021-07-01 16:01:30 +0200

    Update Gym Locale

--- pages/display/gym.php
+++ pages/display/gym.php
@@ -117,7 +117,7 @@
                                 ?>
                             <!-- Card -->
                             <div class="col-lg-3 col-md-3 col-sm-4 col-6 mb-4">
-                                <div class="card border-top-<?php echo i8ln(get_gym_name($row['team']));?> shadow h-100 py-2">
+                                <div class="card border-top-<?php echo get_gym_name($row['team']);?> shadow h-100 py-2">
                                     <div class="card-body d-flex flex-column justify-content-between">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col">
