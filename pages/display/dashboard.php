4279a7470b08503719c6953a31e5bb147e612449
bbdoc <bbdoc.mail@gmail.com>
2021-07-01 16:10:33 +0200

    Update Dashboard Redirect

--- pages/display/dashboard.php
+++ pages/display/dashboard.php
@@ -241,7 +241,7 @@ foreach ($areas as $key => $area) {
                             <!-- Card -->
                             <?php if (@$disable_gyms <> "True") { ?>
                             <div class="col-lg-3 col-md-3 col-sm-4 col-6 mb-4">
-                                <a href="?type=display&page=nest">
+                                <a href="?type=display&page=gym">
                                 <div class="card border-top-dark shadow h-100 py-2">
                                     <div class="card-body d-flex flex-column justify-content-between">
                                         <div class="row no-gutters align-items-center">
