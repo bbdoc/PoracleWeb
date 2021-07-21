490122997850d3ed77ee7553d1d467d49f088aaa
bbdoc <bbdoc.mail@gmail.com>
2021-07-21 23:37:53 +0200

    Replace spaces with underscores

--- modal/areas_modal.php
+++ modal/areas_modal.php
@@ -44,8 +44,9 @@ if(count(array_keys($areas)) === 1){
         };
         echo "<li><input type='checkbox' name='area_$area_var' id='area_$area_var' $checked/>\n";
 	echo "<label for='area_$area_var' style='width:160px;'><font style='font-size:12px;'>$area</font>\n";
-        if (@$disable_geomap_select <> "True") {
-		echo "<br><img src='.cache/geo_".strtoupper($area)."_".$hash.".png' style='width:100%; max-width=100%;'>";
+	if (@$disable_geomap_select <> "True") {
+		$area = strtoupper(str_replace(' ', '_', $area));
+		echo "<br><img src='.cache/geo_".$area."_".$hash.".png' style='width:100%; max-width=100%;'>";
 	}
 	echo "</label>\n";
         echo "</li>\n";
@@ -81,8 +82,9 @@ if(count(array_keys($areas)) === 1){
             };
             echo "<li><input type='checkbox' name='area_$area_var' id='area_$area_var' $checked/>\n";
 	    echo "<label for='area_$area_var' style='width:160px;'><font style='font-size:12px;'>$area</font>\n";
-            if (@$disable_geomap_select <> "True") {
-                    echo "<br><img src='.cache/geo_".strtoupper($area)."_".$hash.".png' style='width:100%; max-width=100%;'>";
+	    if (@$disable_geomap_select <> "True") {
+		    $area = strtoupper(str_replace(' ', '_', $area));
+                    echo "<br><img src='.cache/geo_".$area."_".$hash.".png' style='width:100%; max-width=100%;'>";
             }
 	    echo "</label>\n";
             echo "</li>\n";
