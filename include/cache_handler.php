053ef9b69fc323432a0c708d4b471fe38a052a33
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-09-07 12:27:12 -0400

    Update cache_handler.php

--- include/cache_handler.php
+++ include/cache_handler.php
@@ -14,8 +14,16 @@ global $file_localePkmnData;
 $file_localePkmnData = "./.cache/localePkmnData_".$locale.".json";
 
 global $repo_poracle;
-$repo_poracle="https://raw.githubusercontent.com/KartulUdus/PoracleJS/develop";
+$repo_poracle="https://raw.githubusercontent.com/KartulUdus/PoracleJS/master/src/util/util.json";
 $repo_poracle_cache="24";
+
+global $repo_monsters;
+$repo_monsters="https://github.com/WatWowMap/Masterfile-Generator/raw/master/master-latest-poracle.json";
+$repo_monsters_cache="24";
+
+global $repo_locales;
+$repo_locales="https://raw.githubusercontent.com/WatWowMap/pogo-translations/master/static/englishRef";
+$repo_locales_cache="24";
 
 global $repo_pogoinfo;
 $repo_pogoinfo = "https://raw.githubusercontent.com/ccev/pogoinfo";
@@ -26,11 +34,11 @@ $img_cache="24";
 // Cache Monsters.json
 
 global $monsters_json;
-if (file_exists($file_monsters) && (filemtime($file_monsters) > (time() - 60 * 60 * $repo_poracle_cache ))) { 
+if (file_exists($file_monsters) && (filemtime($file_monsters) > (time() - 60 * 60 * $repo_monsters_cache ))) { 
     $monsters_json = file_get_contents($file_monsters);
 
 } else { 
-    $monsters_json = file_get_contents($repo_poracle."/src/util/monsters.json");
+    $monsters_json = file_get_contents($repo_monsters);
     file_put_contents($file_monsters, $monsters_json);
 }
 
@@ -41,7 +49,7 @@ if (file_exists($file_util) && (filemtime($file_util) > (time() - 60 * 60 * $rep
     $grunts_json = file_get_contents($file_util);
 
 } else { 
-    $grunts_json = file_get_contents($repo_poracle."/src/util/util.json");
+    $grunts_json = file_get_contents($repo_poracle);
     file_put_contents($file_util, $grunts_json);
 }
 
@@ -71,13 +79,13 @@ if (file_exists($file_nest_species) && (filemtime($file_nest_species) > (time() 
 // Cache pokemonNames locale file
 
 global $localePkmnData_json;
-if (file_exists($file_localePkmnData) && (filemtime($file_localePkmnData) > (time() - 60 * 60 * $repo_poracle_cache ))) { 
+if (file_exists($file_localePkmnData) && (filemtime($file_localePkmnData) > (time() - 60 * 60 * $repo_locales_cache ))) { 
     $localePkmnData_json = file_get_contents($file_localePkmnData);
-} else if ( @fopen($repo_poracle."/src/util/locale/pokemonNames_".$locale.".json", 'r') ) { 
-    $localePkmnData_json = file_get_contents($repo_poracle."/src/util/locale/pokemonNames_".$locale.".json");
+} else if ( @fopen($repo_locales."_".$locale.".json", 'r') ) { 
+    $localePkmnData_json = file_get_contents($repo_locales."pokemon_".$locale.".json");
     file_put_contents($file_localePkmnData, $localePkmnData_json);
 } else if (isset($locale)) {
-    $localePkmnData_json = file_get_contents($repo_poracle."/src/util/locale/pokemonNames_en.json");
+    $localePkmnData_json = file_get_contents($repo_locales."pokemon_en.json");
 }	
 
 ?>
